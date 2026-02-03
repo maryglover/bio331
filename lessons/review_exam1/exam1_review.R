8414ed13f2d03dd856b274b758c391e474a2cbef
mglover <mglover@bowiestate.edu>
2026-02-03 07:52:31 -0500

    review

---
+++ lessons/review_exam1/exam1_review.R
@@ -0,0 +1,78 @@
+# Record all of your code used to complete the exam in this R script. At the end of the exam, you will save the script and submit on Moodle along with your printed worksheet.
+
+#### #### #### #### #### 
+#### Introduction  ####
+#### #### #### #### #### 
+
+## question 1
+
+## question 2
+
+## question 3
+
+## question 4
+
+## question 5
+
+
+########################## 
+# Working with a dataset #
+########################## 
+
+## question 1
+
+## question 2
+
+## question 3
+
+## question 4
+
+## question 5
+
+## question 6
+
+## question 7
+
+################################
+## Manipulating the dataset ##
+###############################
+
+## question 1
+
+## question 2
+
+## question 3
+
+## question 4
+
+## question 5
+
+## question 6
+
+## question 7
+
+## question 8
+
+## question 9
+
+## question 10
+
+###########################
+### Graphing the data ###
+###########################
+
+## question 2
+
+## question 3
+
+## question 4
+
+## question 5
+
+## question 6
+
+## question 7
+
+## question 8
+
+## question 9
