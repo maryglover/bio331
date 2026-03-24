f20e7ddb9ae46b8431fa31515f0c3f59a7863f4b
maryglover <maryglover6@gmail.com>
2026-03-24 22:29:55 +0000

    fix data

--- impervious_surface/impervious_surface_data.R
+++ impervious_surface/impervious_surface_data.R
@@ -11,8 +11,8 @@ county <- st_transform(county, crs = 4269)
 imp2021 <- get_nlcd(template = county, label = 'wake', year = 2021, dataset = 'impervious')
 
 streams <- read.csv('open_data/stream_codes.csv')
-raster::extract(impervious, streams[,c(4,3)], buffer = 500, fun = mean)
-raster::extract(imp2021, streams[,c(4,3)], buffer = 500, fun = mean)
+#raster::extract(impervious, streams[,c(4,3)], buffer = 500, fun = mean)
+#raster::extract(imp2021, streams[,c(4,3)], buffer = 500, fun = mean)
 
 
 # sf points
@@ -58,18 +58,71 @@ impervious_data <- streams_imperv_08|>
   separate(name, c('imp', 'year'), sep = '_' )|>
   select(-ID, - imp)
   
-wq <- read.csv('data/raleigh_water_analysis.csv')
-head(wq)
+write.csv(impervious_data, 'impervious_surface/impervious_surface.csv')
+
+wq_rank <- read.csv('water_quality_manip/water_quality_score.csv')
+head(wq_rank)
 library(lubridate)
 
-wq_imp2 <- wq |>
+wq_imp2 <- wq_rank |>
   separate(Date, into = c('year', 'month', 'day'), sep = '-', remove = F) |>
-  full_join(impervious_data) 
+  full_join(impervious_data) |>
+  select(-lat, -long, -year, -month, -day) |>
+  left_join(streams)
 
 write.csv(wq_imp2, 'impervious_surface/raleigh_water_impervious.csv', row.names = F)
-  
+
+# what site has the highest impervious surface in 2021
+
+wq_imp2 |>
+  separate(Date, into = c('year', 'month', 'day'), sep = '-', remove = F) |>
+  filter(year == 2021) |>
+  distinct(Site, Stream, impervious_surface) |>
+  arrange(-impervious_surface)
+
+# average wq_score and impervious surface
+# in 2021
+
+wq_imp2 |>
+  separate(Date, into = c('year', 'month', 'day'), sep = '-', remove = F) |>
+  filter(year == 2021) |>
+  group_by(Site, Stream) |>
+  summarize(wq = mean(wq_score, na.rm = T), impervious = mean(impervious_surface))|>
+  arrange(-wq)
+
+# overall
+wq_imp2 |>
+  group_by(Site, Stream) |>
+  summarize(wq = mean(wq_score, na.rm = T), impervious = mean(impervious_surface, na.rm = T))|>
+  arrange(-wq)
+
+
+ggplot(wq_imp2, aes(x = impervious_surface, y = wq_score))+
+  geom_point() +
+  geom_smooth()
+
 ggplot(wq_imp2, aes(x = impervious_surface, y = NO2_NO3_mg_L)) +
-  geom_point()
+  geom_point() 
+
+ggplot(wq_imp2, aes(x = impervious_surface, y = Turbidity_NTU)) +
+  geom_point() 
+ggplot(wq_imp2, aes(x = impervious_surface, y = E_coli_MPN_100mL)) +
+  geom_point() 
+
+wq_imp2 |>
+  separate(Date, into = c('year', 'month', 'day'), sep = '-', remove = F) |>
+  group_by(year) |>
+  summarise(imp = mean(impervious_surface, na.rm = T)) |>
+  filter(imp > 1)
+ 
+wq_imp2<- wq_imp2 |>
+  separate(Date, into = c('year', 'month', 'day'), sep = '-', remove = F) 
+
+wq_imp2 |>
+  filter(year %in% c(2008, 2011, 2013, 2016, 2019, 2021)) |>
+  distinct(Site, year, impervious_surface) |>
+  ggplot(aes(x = year, y = impervious_surface, group = Site)) +
+  geom_line()
 
 ggplot()+
   geom_spatraster(data = imp2021) +
